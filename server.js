c48967bbf5ce7a38de4a9969aad6652e4a18b8f0
chiefkeef11 <firma034@gmail.com>
2026-05-20 00:18:12 +0300

    fix uploads folder

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const cors = require("cors");
 const multer = require("multer");
 const { Pool } = require("pg");
